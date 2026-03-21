35911e3133f3e1983a9957156e40a8726033600f
Johan Janérs <johanjaners@gmail.com>
2026-03-21 21:26:20 +0100

    Revert "chore: add Swashbuckle for API documentation and configure Swagger UI"
    This reverts commit 868847d090a1280457f167c20e5b3024c270b501.

--- note2quiz-backend/Note2Quiz.API/Program.cs
+++ note2quiz-backend/Note2Quiz.API/Program.cs
@@ -43,7 +43,6 @@ builder.Services.AddSingleton<IChatClient>(sp =>
 
     return new AzureChatClient(endpoint, apiKey, deployment);
 });
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(
@@ -78,10 +77,7 @@ builder
             new System.Text.Json.Serialization.JsonStringEnumConverter()
         );
     });
-
 builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder
     .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -107,9 +103,6 @@ var app = builder.Build();
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.MapOpenApi();
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
